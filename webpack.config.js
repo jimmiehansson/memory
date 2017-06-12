1b0337cce0840139cd48fec37d57d4d4852d6a34
Jimmie Hansson <jimmiehansson1985@gmail.com>
2017-06-12 05:09:14 +0200

    Final v0.0.1, production build

--- webpack.config.js
+++ webpack.config.js
@@ -54,8 +54,6 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-
-        /*
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
@@ -64,6 +62,5 @@ module.exports = {
                 warnings: false
             }
         })
-        */
     ]
 };
